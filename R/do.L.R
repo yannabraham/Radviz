f3c6d016231cd0850c9df24bb7b213035f40b940
Yann Abraham <yann.abraham@gmail.com>
2015-11-27 09:13:36 +0100

    updated the do.L function to take a fun argument: the range to normalize to will be defined by fun, rather than min and max.

--- R/do.L.R
+++ R/do.L.R
@@ -3,6 +3,8 @@
 #' Standardizes all values in a vector to the unit vector ([0,1]) using local min and max
 #' 
 #' @param v a vector of values
+#' @param fun a function that will return the minimum and maximum values to use to scale v;
+#'          defaults to \code{\link[base]{range}}
 #' @param na.rm Logical: should NA be removed? defaults to \code{TRUE}
 #' 
 #' @details This is an alternative to performing a L normalization over the full matrix. 
@@ -17,12 +19,21 @@
 #' 
 #' @author Yann Abraham
 #' @export
-do.L <-
-function(v,na.rm=T) {
-	if(na.rm) {
-		v <- v[!is.na(v)]
+do.L <- function(v,fun=range,na.rm=T) {
+	# if requested remove the na values
+  if(na.rm) {
+		clean_v <- v[!is.na(v)]
+	} else {
+	  clean_v <- v
 	}
-	min_v <- min(v)
-	max_v <- max(v)
-	return((v-min_v)/(max_v-min_v))
+  # find the min and the max
+  range_v <- match.fun(fun)(clean_v)
+	min_v <- min(range_v)
+	max_v <- max(range_v)
+	# scale v
+	scaled_v <- (v-min_v)/(max_v-min_v)
+	# remove values above and below 1 and 0
+	scaled_v[scaled_v<0] <- 0
+	scaled_v[scaled_v>1] <- 1
+	return(scaled_v)
 }
