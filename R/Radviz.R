a818bf21e85d44f2a395ed6d2f331e51e14beb42
Yann Abraham <yann.abraham@gmail.com>
2019-12-13 19:01:04 +0100

    some functions are now defunct as a consequence of the shift to ggplot2

--- R/Radviz.R
+++ R/Radviz.R
@@ -3,13 +3,18 @@
 #' Radviz uses Dimensional Anchors and the spring paradigm to project a multidimensional space in 2D.
 #' This allows for the quick visualization of large and complex datasets.
 #' 
+#' @example examples/example-do.radviz.R
 #' @examples
-#' data(iris)
-#' das <- c('Sepal.Length','Sepal.Width','Petal.Length','Petal.Width')
-#' S <- make.S(das)
-#' rv <- do.radviz(iris,S)
-#' plot(rv,point.shape=1,point.color=c('red','green','blue')[as.integer(iris$Species)])
+#' plot(rv,anchors.only=FALSE)
 #' 
 #' @docType package
 #' @name Radviz
+NULL
+
+#' Defunct Functions
+#'
+#' These functions are Defunct in this release of Radviz, they will be
+#' marked as Defunct and removed in a future version.
+#' @name Radviz-deprecated
+#' @keywords internal
 NULL
--- R/do.density.R
+++ R/do.density.R
@@ -1,61 +1,7 @@
-#' Computes 2D density for contour plots of Radviz objects
-#' 
-#' Computes 2D density estimate of projected data for a Radivz object,
-#' using the \code{\link[MASS]{kde2d}} function from the \pkg{MASS} package
-#' 
-#' @param x a radviz object as produced by do.radviz
-#' @param n Number of grid points in each direction
-#' @param method the method to use for KDE estimation
-#' @param bandwidth the bandwidth to use for \link[KernSmooth]{bkde2D}
-#' 
-#' @details Computes a 2D density estimate of Radviz projected data and stores the results
-#'            in a \code{density} slot of the Radviz object. Invalid points, if any, will be
-#'            excluded.
-#'            2 methods are implemented, \link[MASS]{kde2d} for \code{\link{contour.radviz}} and 
-#'            \link[KernSmooth]{bkde2D} for \code{\link{smoothRadviz}}
-#' 
-#' @return the Radviz object with and extra slot \code{density} containing the 2D density
-#' estimates for use with \code{\link{contour.radviz}} or \code{\link{smoothRadviz}}
-#' 
-#' @examples
-#' data(iris)
-#' das <- c('Sepal.Length','Sepal.Width','Petal.Length','Petal.Width')
-#' S <- make.S(das)
-#' rv <- do.radviz(iris,S)
-#' rv <- do.density(rv)
-#' rv <- do.density(rv,n=128,method='KernSmooth')
-#' contour(rv,point.shape=1,point.color=c('red','green','blue')[as.integer(iris$Species)])
-#' smoothRadviz(rv)
-#' 
-#' @seealso \link{contour.radviz} for plotting
-#' @author Yann Abraham
+#' @rdname Radviz-deprecated
 #' @export
-do.density <- function(x,n=50,method='MASS',bandwidth) {
-  if(any(!x$valid)) {
-    warning(sum(!x$valid)," point(s) could not be projected, which will not be used to compute density")
-  }
-  if(!method %in% c('MASS','KernSmooth')) {
-    stop(method,' is not a valid method; see help(do.density) for valid options.')
-  }
-  if(method=='MASS') {
-    x$contour <- MASS::kde2d(x$projected[x$valid,'x'],x$projected[x$valid,'y'],
-                             n=n,
-                             lims=c(c(-1.1,1.1),c(-1.1,1.1)))
-  } else if(method=='KernSmooth') {
-    if (missing(bandwidth)) {
-      bandwidth <- diff(apply(x$projected[x$valid,], 2, quantile, probs = c(0.05, 0.95), names = FALSE))/25
-      bandwidth[bandwidth == 0] <- 1
-    }
-    else {
-      if (!is.numeric(bandwidth)) 
-        stop("'bandwidth' must be numeric")
-      if (any(bandwidth <= 0)) 
-        stop("'bandwidth' must be positive")
-    }
-    x$smooth <- KernSmooth::bkde2D(x$projected[x$valid,],
-                                   bandwidth = bandwidth,
-                                   gridsize = c(n,n),
-                                   range.x = list(c(-1.1,1.1),c(-1.1,1.1)))
-  }
+do.density <- function(x,...) {
+  .Defunct(new='contour.radviz',
+           msg='the do.density function is not required anymore, use contour.radviz directly')
   return(x)
 }
