0034178bd3139383fb2173d3caa73b62a03f48fd
Yann Abraham <yann.abraham@gmail.com>
2019-12-16 08:28:23 +0100

    the plot function now shows only the anchors by default, and returns the underlying ggplot object for addition of further geoms

--- R/plot.radviz.R
+++ R/plot.radviz.R
@@ -4,46 +4,40 @@
 #' 
 #' @param x a radviz object as produced by \code{\link{do.radviz}}
 #' @param main [Optional] a title to the graph, displayed on top
-#' @param label.color The color of the Dimensional Anchors (defaults to orangered4)
-#' @param label.size numeric character expansion factor for Dimensional Anchor labels;
-#' multiplied by \code{par("cex")} yields the final character size. NULL and NA are equivalent to 1.0
-#' @param point.color The point color (defaults to black)
-#' @param point.shape The point shape (defaults to '.')
-#' @param point.size The point size (defaults to 1)
-#' @param add Logical: if add is \code{TRUE} then only the projected points are plotted
-#' @param anchors.only plot only the anchors so that other plots can be freely overlaid
-#' @param ...	further arguments to be passed to or from other methods
+#' @param anchors.only by default only plot the anchors so that other methods can easily be chained
+#' @param ...	further arguments to be passed to or from other methods (not implemented)
 #' 
-#' @details The \code{add} option allows plotting of additional data
-#' such as cluster centers onto an existing plot.
+#' @details by default the plot function only shows the anchors. Extra geoms are required to display the data
 #' 
+#' @return the internal ggplot2 object, allowing for extra geoms to be added
+#' 
+#' @example examples/example-do.radviz.R
 #' @examples
-#' data(iris) 
-#' das <- c('Sepal.Length','Sepal.Width','Petal.Length','Petal.Width')
-#' S <- make.S(das)
-#' rv <- do.radviz(iris,S)
-#' plot(rv,point.shape=1,point.color=c('red','green','blue')[as.integer(iris$Species)])
+#' plot(rv)
+#' plot(rv,anchors.only=FALSE)
+#' 
+#' library(ggplot2)
+#' ## should look the same as before
+#' plot(rv)+geom_point()
+#' plot(rv)+geom_point(aes(color=Species))
 #' 
 #' @author Yann Abraham
-#' @importFrom graphics par plot text points
+#' @importFrom ggplot2 ggtitle geom_point
 #' @export
-plot.radviz <- function(x,main=NULL,
-                        label.color='orangered4',label.size=1,
-                        point.color='black',point.shape='.',point.size=1,
-                        add=FALSE,anchors.only=FALSE,...) {
-  par(mar = c(0,0,1,0))
-  if (!add) {
-    plot(x$springs, type = "n", main = main, xlab = "", 
-         ylab = "", xlim = c(-1.1, 1.1), ylim = c(-1.1, 1.1), 
-         frame.plot = F, axes = F)
-    text(x$springs, labels = dimnames(x$springs)[[1]], 
-         col = label.color,cex=label.size)
+plot.radviz <- function(x,
+                        main=NULL,
+                        anchors.only=TRUE,
+                        ...) {
+  if(!is.null(main)) {
+    p <- x$proj+
+      ggtitle(main)
+  } 
+  if(anchors.only) { 
+    p <- x$proj
   }
-  if(!anchors.only) {
-    points(x$projected[x$valid,], pch = point.shape, col = point.color,
-           cex = point.size)
+  else {
+    p <- x$proj+
+      geom_point()
   }
-  if (add & anchors.only) {
-    warning('add and anchors.only are both TRUE, so nothing will be plotted')
-  }
+  return(p)
 }
