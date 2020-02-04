3eb07b272d438e98c0618c164dabcfdad5ccc674
Nicolas Sauwen <nsauwen@its.jnj.com>
2020-02-04 17:33:17 +0100

    DB_weightedIdx() added + update documentation

---
+++ R/DB_weightedIdx.R
@@ -0,0 +1,34 @@
+
+#' Computation of weighted version of the Davies-Bouldin index. This index serves as a measure of clustering quality of a 2D projection result with known class labels
+#' 
+#' @param x an object of class Radviz, as returned by \code{\link{do.freeviz}}. Note that the DB weighted index can only be computed for Radviz objects with slot \code{type} = "freeviz"
+#' @return weighted DB index value
+#' 
+#' @author Nicolas Sauwen
+#' @export
+DB_weightedIdx <- function(x){
+	
+	data <- x$data
+	springs <- x$springs
+	classes <- x$classes
+	
+	if(is.null(classes)){
+		stop("Davies-Bouldin weighted index can only be computed on radviz object of type 'freeviz'")
+	}
+	
+	classes <- as.integer(as.factor(classes))
+	nClasses <- length(unique(classes))
+	clusterSizes <- table(classes)
+	
+	dataNormalized <- apply(data,2, do.L) 
+	projectedData <- dataNormalized%*%springs
+	
+	if(!requireNamespace("clusterSim", quietly = FALSE)) install.packages("clusterSim")	
+	DBResult <- clusterSim::index.DB(projectedData, classes)
+#	R_vals <- DBResult$r
+#	DBIdx <- sum(R_vals*clusterSizes)/sum(clusterSizes)
+	meanRVect <- unlist(lapply(1:nClasses, function(k) mean(DBResult$R[k,][-k])))
+	DBIdx <- sum(table(classes)*meanRVect)/sum(table(classes))
+	
+	return(DBIdx)
+}
