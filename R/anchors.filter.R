52cf8684d6c60826341e3e220f1e318fc4513dbe
yann.abraham@gmail.com <yann.abraham@gmail.com>
2020-11-13 12:01:06 +0100

    implemented `anchor.filter` as a separate function so that it can be used across the different types of plot

---
+++ R/anchors.filter.R
@@ -0,0 +1,29 @@
+#' Filtering out anchors with low contributions to the projection
+#' 
+#' @param x a radviz object as produced by \code{\link{do.radviz}}
+#' @param lim the minimum length of an anchor
+#' 
+#' @details When \code{anchors.filter} is a number and type is not Radviz, any springs whose length is lower than this number will be filtered out 
+#' of the visualization. This has no effect on the projection itself.
+#' 
+#' @return a radviz object as produced by \code{\link{do.radviz}}
+#' 
+#' @example examples/example-do.radviz.R
+#' @examples
+#' plot(rv,anchors.only=FALSE)
+#' new.S <- do.optimFreeviz(x = iris[,das], classes = iris$Species)
+#' new.rv <- do.radviz(iris,new.S)
+#' plot(new.rv,anchors.only=FALSE)
+#' plot(anchors.filter(new.rv,0.2))
+#' 
+anchors.filter <- function(x,
+                           lim=0) {
+  if(x$type=='Radviz') {
+    warning('`anchors.filter` is not relevant for Radviz plots\n')
+  } else {
+    x$proj$layers[[1]]$data <- x$proj$layers[[1]]$data %>% 
+      mutate(weight=(X1^2+X2^2)^0.5) %>% 
+      subset(weight>=lim)
+  }
+  return(x)
+}
